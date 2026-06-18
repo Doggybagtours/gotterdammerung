9282aa952bab0c8abede3e7c6f171bb54286ef4c
Doggy Bag Tours <doggybagtours@gmail.com>
2026-06-19 00:45:09 +0200

    Simplify icon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,25 +19,8 @@ export const metadata: Metadata = {
   description: siteConfig.description,
   metadataBase: new URL(siteConfig.url),
   icons: {
-    icon: [
-      {
-        url: "/favicon/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/favicon/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-    ],
-    apple: [
-      {
-        url: "/favicon/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
+    icon: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
   },
   openGraph: {
     title: "Götterdämmerung",
